7e5e88f57568760a7850d9de4cc7a0dfb6ab4761
BaranovSev <aldebaran94@mail.ru>
2022-08-14 14:38:05 +0300

    images to micropost added; prepearing for  iamge resizing

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,19 @@
+require "image_processing/mini_magick"
+
 class ImageUploader < Shrine
   Attacher.validate do
     validate_size      1..5*1024*1024 # 5 MB maximum size
     validate_mime_type %w[image/jpeg image/png image/webp image/tiff]
     validate_extension %w[jpg jpeg png webp tiff tif]
+  end
+  
+  Attacher.derivatives do |original|
+    magick = ImageProcessing::MiniMagick.source(original)
+ 
+    { 
+      large:  magick.resize_to_limit!(800, 800),
+      medium: magick.resize_to_limit!(500, 500),
+      small:  magick.resize_to_limit!(300, 300),
+    }
   end
 end
